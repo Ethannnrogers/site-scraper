0a69bba75d36a86dc80d92104634b3541af92e3f
Ethan <erogers999@gmail.com>
2018-04-26 17:07:23 -0400

    push

---
+++ public/app.js
@@ -0,0 +1,84 @@
+// Grab the articles as a json
+$.getJSON("/articles", function(data) {
+    // For each one
+    for (var i = 0; i < data.length; i++) {
+      // Display the apropos information on the page
+      $("#articles").append("<div class='container article-pop'><h4 class ='news-title-link'data-id='" + data[i]._id + "'>" + data[i].title + "</h4><button class='btn btn-info'><a target='_blank' id='see-more' href='" + data[i].link + "'>Read Article</a></button></div>");
+    }
+  });
+
+
+//Hide content
+$("#wrapper").hide();
+//show content
+$("#get-news").on("click", function() {
+  $("#wrapper").show();
+})
+
+
+// Whenever someone clicks a p tag
+$(document).on("click", ".news-title-link", function() {
+    // Empty the notes from the note section
+    $("#notes").empty();
+    // Save the id from the p tag
+    var thisId = $(this).attr("data-id");
+  
+    // Now make an ajax call for the Article
+    $.ajax({
+      method: "GET",
+      url: "/articles/" + thisId
+    })
+      // With that done, add the note information to the page
+      .then(function(data) {
+        console.log(data);
+        // The title of the article
+        $("#notes").append("<div class='outline' id='note-title'><h2>" + data.title + "</h2></div>");
+        // An input to enter a new title
+        $("#notes").append("<br><input class='form-control' placeholder='Note Title' id='titleinput' name='title' ><br>");
+        // A textarea to add a new note body
+        $("#notes").append("<textarea id='bodyinput' class='form-control' placeholder='Note Message' name='body'></textarea>");
+        // A button to submit a new note, with the id of the article saved to it
+        $("#notes").append("<br><button class='btn btn-info' data-id='" + data._id + "' id='savenote'>Save Note</button>");
+  
+        // If there's a note in the article
+        if (data.note) {
+          // Place the title of the note in the title input
+          $("#titleinput").val(data.note.title);
+          // Place the body of the note in the body textarea
+          $("#bodyinput").val(data.note.body);
+        }
+      });
+  });
+
+  
+  // When you click the savenote button
+  $(document).on("click", "#savenote", function() {
+    // Grab the id associated with the article from the submit button
+    var thisId = $(this).attr("data-id");
+  
+    // Run a POST request to change the note, using what's entered in the inputs
+    $.ajax({
+      method: "POST",
+      url: "/articles/" + thisId,
+      data: {
+        // Value taken from title input
+        title: $("#titleinput").val(),
+        // Value taken from note textarea
+        body: $("#bodyinput").val()
+      }
+    })
+      // With that done
+      .then(function(data) {
+        // Log the response
+        console.log(data);
+        // Empty the notes section
+        $("#notes").empty();
+      });
+  
+    // Also, remove the values entered in the input and textarea for note entry
+    $("#titleinput").val("");
+    $("#bodyinput").val("");
+  });
+  
+  
+  
---
+++ server.js
@@ -0,0 +1,111 @@
+var express = require("express");
+var handlebars = require("express-handlebars");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var cheerio = require("cheerio");
+var request = require("request");
+var path = require("path");
+var axios = require("axios");
+var logger = require("morgan");
+
+
+var db = require("./models");
+var PORT = 3000;
+const app = express();
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
+
+mongoose.connect("mongodb://localhost/site_scraper");
+
+//Routessssssss
+app.get("/scrape", function(req, res) {
+  //uhh idk what this does
+  axios.get("https://www.nytimes.com/").then(function(response) {
+    //load something to cheerio and save it as $
+    var $ = cheerio.load(response.data);
+    var results = [];
+    $("h2.story-heading").each(function(i, element) {
+      var link = $(element).children().attr("href");
+      var title = $(element).children().text();
+      
+      
+      // Save these results in an object that we'll push into the results array we defined earlier
+      results.push({
+      title: title,
+      link: link
+        });
+      });
+      
+      $("p.summary").each(function(i, element) {
+      
+        var summary = $(element).children().text();
+      
+        results.push({
+          summary: summary
+        });
+      });
+
+      db.Articles.create(results)
+      .then(function(dbArticles) {
+        console.log(dbArticles);
+      })
+      .catch(function(err) {
+        return res.json(err);
+      });
+      
+      res.send("Scrape Complete");
+    });
+  });
+
+
+  //Route for getting the articles
+  app.get("/articles", function(req, res) {
+    db.Articles.find({}).then(function(dbArticles) {
+      res.json(dbArticles);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+  });
+
+
+  //Route for grabbing a specific Article by id\
+app.get("/articles/:id", function(req, res) {
+  db.Articles.findOne({_id: req.params.id }).populate("notes").then(function(dbArticles) {
+    res.json(dbArticles);
+  })
+  .catch(function(err) {
+    res.json(err);
+  });
+});
+
+
+//Route for saving/updating an Articles notes
+app.post("/articles/:id", function(req, res) {
+  db.Notes.create(req.body).then(function(dbNotes) {
+    return db.Articles.findOneAndUpdate({ _id: req.params.id }, { note: dbNotes._id }, { new: true});
+  })
+  .then(function(dbArticles){
+    res.json(dbArticles);
+  })
+  .catch(function(err) {
+    res.json(err);
+  });
+});
+
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/html/index.html"));
+  });
+
+
+//Server is running
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
